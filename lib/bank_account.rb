3752434fe751bac9e713a16e864c6eae43883f68
Wesley <wes.beck21@outlook.com>
2020-11-24 15:22:32 -0800

    commit tree b073f77cbbd7ec56ebbc99e95446a9e40da50fcc

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
+attr_reader :name
+attr_accessor :balance, :status
+
+    def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    end
+
+    def deposit(money)
+        @balance = balance + "#{money}".to_i
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+
+    end
+
+    def valid?
+        self.balance > 0 && self.status == "open" ? true : false
+    end
+
+    def close_account
+        self.status = "closed"
+
+
+    end
 
 end
